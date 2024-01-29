c509879b1019f019f37750005d3d4e45132091de
Алексей Журавлев <dralex1982@gmail.com>
2024-01-29 22:53:29 +0200

    Add products.ts

---
+++ src/modules/cart/data/fake/products.ts
@@ -0,0 +1,21 @@
+import imgProduct1 from "../../../cart/presentation/page/img/1.png";
+import imgProduct2 from "../../../cart/presentation/page/img/2.png";
+import imgProduct3 from "../../../cart/presentation/page/img/3.png";
+
+export const products = [
+    {
+        name: "Tray Table",
+        color: "black",
+        image: imgProduct1
+    },
+    {
+        name: "Tray Table",
+        color: "black",
+        image: imgProduct2
+    },
+    {
+        name: "Table lamp",
+        color: "Gold",
+        image: imgProduct3
+    }
+]
--- src/modules/cart/presentation/page/cart_checkout/cart_checkout.tsx
+++ src/modules/cart/presentation/page/cart_checkout/cart_checkout.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ContactInformation from "./ContactInformation/ContactInformation";
 import Address from "./Address/Address";
 import PaymentMethod from "./PaymentMethod/PaymentMethod";
-import OrderSummary from "./OrderSummary/OrderSummary";
+import Cart_productCard from "../cart_productCard/Cart_productCard";
 
 const CartCheckout = () => {
     return (
@@ -10,7 +10,7 @@ const CartCheckout = () => {
             <ContactInformation/>
             <Address/>
             <PaymentMethod/>
-            <OrderSummary/>
+            <Cart_productCard/>
             <button>Place Order</button>
         </div>
     );

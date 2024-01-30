1230adeadef738809da33df3ada2d02c256cf3fa
Алексей Журавлев <dralex1982@gmail.com>
2024-01-30 23:43:24 +0200

    Create cart_checkout.module.css edit forms Address.tsx, ContactInformation.tsx

--- src/modules/cart/presentation/page/cart_checkout/Address/Address.tsx
+++ src/modules/cart/presentation/page/cart_checkout/Address/Address.tsx
@@ -1,36 +1,44 @@
 import React from 'react';
+import s from '../cart_checkout.module.css'
 
 const Address = () => {
     return (
-        <div>
-            <form>
-                <label>Shipping Address</label>
-                <p>
-                    <label>Street Address*</label>
-                    <input type={"text"} name={"address"} placeholder={"Street Address"}/>
-                </p>
-                <p>
-                    <label>Country*</label>
-                    <input type={"text"} name={"country"} placeholder={"Country"}/>
-                </p>
-                <p>
-                    <label>Town/City</label>
-                    <input type={"text"} name={"city"} placeholder={"Town/City"}/>
-                </p>
-                <p>
-                    <label>State</label>
-                    <input type={"text"} name={"state"} placeholder={"State"}/>
-                </p>
-                <p>
-                    <label>Zip Code</label>
-                    <input type={"number"} name={"zipCode"} placeholder={"Zip Code"}/>
-                </p>
-                <p>
+            <form className={s.cartForm}>
+                <label className={s.cartForm__textTitle}>Shipping Address</label>
+                <div className={s.cartForm__item}>
+                    <label className={s.input__labelText}>Street Address*</label>
+                    <input className={s.cartForm__input + ' ' + s.cartForm__inputWhole}
+                           type={"text"} name={"address"} placeholder={"Street Address"}/>
+                </div>
+                <div className={s.cartForm__item}>
+                    <label className={s.input__labelText}>Country*</label>
+                    <input className={s.cartForm__input + ' ' + s.cartForm__inputWhole}
+                           type={"text"} name={"country"} placeholder={"Country"}/>
+                </div>
+                <div className={s.cartForm__item}>
+                    <label className={s.input__labelText}>Town/City</label>
+                    <input className={s.cartForm__input + ' ' + s.cartForm__inputWhole}
+                           type={"text"} name={"city"} placeholder={"Town/City"}/>
+                </div>
+                <div className={s.cartForm__rowForTwo}>
+                    <div className={s.cartForm__item}>
+                        <label className={s.input__labelText}>State</label>
+                        <select className={s.cartForm__input + ' ' + s.cartForm__inputHalf}
+                               name={"state"}>
+                            <option>State</option>
+                        </select>
+                    </div>
+                    <div className={s.cartForm__item}>
+                        <label className={s.input__labelText}>Zip Code</label>
+                        <input className={s.cartForm__input + ' ' + s.cartForm__inputHalf}
+                               type={"number"} name={"zipCode"} placeholder={"Zip Code"}/>
+                    </div>
+                </div>
+                <div className={s.cartForm__rowForCheckBox}>
                     <input type={"checkbox"} name={"isDifferentbillingAddress"}/>
-                    <label>Use a different billing address (optional)</label>
-                </p>
+                    <label className={s.cartForm__textForCheckBox}>Use a different billing address (optional)</label>
+                </div>
             </form>
-        </div>
     );
 };
 
--- src/modules/cart/presentation/page/cart_checkout/ContactInformation/ContactInformation.tsx
+++ src/modules/cart/presentation/page/cart_checkout/ContactInformation/ContactInformation.tsx
@@ -1,28 +1,35 @@
 import React from 'react';
+import s from '../cart_checkout.module.css';
 
 const ContactInformation = () => {
     return (
-        <div>
-            <form className={"cartForm"}>
-                <label className={"CartFormTitle"}>Contact Information</label>
-                <p className={"CartFormItemName"}>
-                    <label>First Name</label>
-                    <input type={"text"} name={"firstName"} placeholder={'First Name'}/>
-                </p>
-                <p className={"CartFormItemName"}>
-                    <label>Last Name</label>
-                    <input type={"text"} name={"lastName"} placeholder={'Last Name'}/>
-                </p>
-                <p className={"CartFormItemAddress"}>
-                    <label>Phone Number</label>
-                    <input type={"text"} name={"PhoneNumber"} placeholder={'Phone number'}/>
-                </p>
-                <p className={"CartFormItemAddress"}>
-                    <label>Email address</label>
-                    <input type={"text"} name={"firstName"} placeholder={'Your Email'}/>
-                </p>
-            </form>
-        </div>
+        <form className={s.cartForm}>
+            <label className={s.cartForm__textTitle}>
+                Contact Information
+            </label>
+            <div className={s.cartForm__rowForTwo}>
+                <div className={s.cartForm__item}>
+                    <label className={s.input__labelText}>First Name</label>
+                    <input className={s.cartForm__input + ' ' + s.cartForm__inputHalf}
+                           type={"text"} name={"firstName"} placeholder={'First Name'}/>
+                </div>
+                <div className={s.cartForm__item}>
+                    <label className={s.input__labelText}>Last Name</label>
+                    <input className={s.cartForm__input + ' ' + s.cartForm__inputHalf}
+                           type={"text"} name={"lastName"} placeholder={'Last Name'}/>
+                </div>
+            </div>
+            <div className={s.cartForm__item}>
+                <label className={s.input__labelText}>Phone Number</label>
+                <input className={s.cartForm__input + ' ' + s.cartForm__inputWhole}
+                       type={"text"} name={"PhoneNumber"} placeholder={'Phone number'}/>
+            </div>
+            <div className={s.cartForm__item}>
+                <label className={s.input__labelText}>Email address</label>
+                <input className={s.cartForm__input + ' ' + s.cartForm__inputWhole}
+                       type={"text"} name={"firstName"} placeholder={'Your Email'}/>
+            </div>
+        </form>
     );
 };
 

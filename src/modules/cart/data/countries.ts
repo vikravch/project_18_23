ae9fc0fe13880857f1347ff7d4197269b584cd66
Алексей Журавлев <dralex1982@gmail.com>
2024-01-31 12:10:00 +0200

    add countries.ts edit Address.tsx

---
+++ src/modules/cart/data/countries.ts
@@ -0,0 +1,7 @@
+export const countries: string[] = [
+    'Israel',
+    'USA',
+    'France',
+    'Ireland',
+    'Portugal'
+]
--- src/modules/cart/presentation/page/cart_checkout/Address/Address.tsx
+++ src/modules/cart/presentation/page/cart_checkout/Address/Address.tsx
@@ -1,44 +1,51 @@
 import React from 'react';
 import s from '../cart_checkout.module.css'
+import {countries} from "../../../../data/countries";
 
 const Address = () => {
     return (
-            <form className={s.form}>
-                <label className={s.form__textTitle}>Shipping Address</label>
+        <form className={s.form}>
+            <label className={s.form__textTitle}>Shipping Address</label>
+            <div className={s.form__containerInput}>
+                <label className={s.form__inputTextLabel}>Street Address*</label>
+                <input className={s.form__inputContainer + ' ' + s.form__inputWholeContainer}
+                       type={"text"} name={"address"} placeholder={"Street Address"}/>
+            </div>
+            <div className={s.form__containerInput}>
+                <label className={s.form__inputTextLabel}>Country*</label>
+                <select className={s.form__inputContainer + ' ' + s.form__inputWholeContainer}
+                        name={"country"}>
+                    <option value={'0'}>Country</option>
+                    {
+                        countries.map((c: string, index: number) =>
+                            <option value={index}>{c}</option>
+                        )
+                    }
+                </select>
+            </div>
+            <div className={s.form__containerInput}>
+                <label className={s.form__inputTextLabel}>Town/City</label>
+                <input className={s.form__inputContainer + ' ' + s.form__inputWholeContainer}
+                       type={"text"} name={"city"} placeholder={"Town/City"}/>
+            </div>
+            <div className={s.form__rowForTwoInput}>
                 <div className={s.form__containerInput}>
-                    <label className={s.form__inputTextLabel}>Street Address*</label>
-                    <input className={s.form__inputContainer + ' ' + s.form__inputWholeContainer}
-                           type={"text"} name={"address"} placeholder={"Street Address"}/>
+                    <label className={s.form__inputTextLabel}>State</label>
+                    <input className={s.form__inputContainer + ' ' + s.form__inputHalfContainer}
+                           type={"text"} name={"state"} placeholder={"State"}/>
                 </div>
                 <div className={s.form__containerInput}>
-                    <label className={s.form__inputTextLabel}>Country*</label>
-                    <select className={s.form__inputContainer + ' ' + s.form__inputWholeContainer}
-                            name={"country"}>
-                        <option>Country</option>
-                    </select>
+                    <label className={s.form__inputTextLabel}>Zip Code</label>
+                    <input className={s.form__inputContainer + ' ' + s.form__inputHalfContainer}
+                           type={"text"} name={"zipCode"} placeholder={"Zip Code"}/>
                 </div>
-                <div className={s.form__containerInput}>
-                    <label className={s.form__inputTextLabel}>Town/City</label>
-                    <input className={s.form__inputContainer + ' ' + s.form__inputWholeContainer}
-                           type={"text"} name={"city"} placeholder={"Town/City"}/>
-                </div>
-                <div className={s.form__rowForTwoInput}>
-                    <div className={s.form__containerInput}>
-                        <label className={s.form__inputTextLabel}>State</label>
-                        <input className={s.form__inputContainer + ' ' + s.form__inputHalfContainer}
-                               type={"text"} name={"state"} placeholder={"State"}/>
-                    </div>
-                    <div className={s.form__containerInput}>
-                        <label className={s.form__inputTextLabel}>Zip Code</label>
-                        <input className={s.form__inputContainer + ' ' + s.form__inputHalfContainer}
-                               type={"text"} name={"zipCode"} placeholder={"Zip Code"}/>
-                    </div>
-                </div>
-                <div className={s.form__checkBoxContainer}>
-                    <input type={"checkbox"} name={"isDifferentbillingAddress"}/>
-                    <label className={s.form__checkBoxContainer_textElement}>Use a different billing address (optional)</label>
-                </div>
-            </form>
+            </div>
+            <div className={s.form__checkBoxContainer}>
+                <input type={"checkbox"} name={"isDifferentbillingAddress"}/>
+                <label className={s.form__checkBoxContainer_textElement}>Use a different billing address
+                    (optional)</label>
+            </div>
+        </form>
     );
 };
 

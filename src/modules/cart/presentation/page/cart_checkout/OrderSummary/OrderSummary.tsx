7bc973d4672df0efb17feb517cb0c8b17d59ca3b
Алексей Журавлев <dralex1982@gmail.com>
2024-01-31 13:32:28 +0200

    edit cart_checkout.module.css, OrderSummary.tsx

--- src/modules/cart/presentation/page/cart_checkout/OrderSummary/OrderSummary.tsx
+++ src/modules/cart/presentation/page/cart_checkout/OrderSummary/OrderSummary.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import s from "../cart_checkout.module.css";
 import {products} from "../../../../data/fake/products-config";
 import {ProductType} from "../../../../data/types";
+import minus from "../../img/minus.png";
+import plus from "../../img/plus.png";
+import remove from "../../img/remove.png";
 
 const OrderSummary = () => {
     return (
@@ -13,9 +16,26 @@ const OrderSummary = () => {
                 <div className={s.productItem} key={index}>
                     <img className={s.productItem__image} src={p.image}/>
                     <div className={s.productItem__summary}>
-                        <p className={s.productItem__textTitle}>{p.title}</p>
-                        <p className={s.productItem__textColor}>Color: {p.color}</p>
-                        <p className={s.productItem__textPrice}>${p.cost}</p>
+                        <div className={s.productItem__summary_rowForTwo}>
+                            <p className={s.productItem__textTitle}>{p.title}</p>
+                            <p className={s.productItem__textPrice}>${p.cost}</p>
+                        </div>
+                        <div className={s.productItem__summary_rowForTwo}>
+                            <p className={s.productItem__textColor}>Color: {p.color}</p>
+                            <img className={s.productItem__removeBtn}
+                                 src={remove}/>
+                        </div>
+                        <div className={s.productItem__summary_rowForTwo}>
+                            <div className={s.productItem__summary_quantity}>
+                                <div className={s.productItem__summary_quantity_content}>
+                                    <img className={s.productItem__summary_quantity_content_btn}
+                                         src={plus}/>
+                                    <p>2</p>
+                                    <img className={s.productItem__summary_quantity_content_btn}
+                                         src={minus}/>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             )}
